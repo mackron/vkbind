f2bf04266e32bb6892ae3f0a14e6e62bce97c786
David Reid <mackron@gmail.com>
2019-10-21 20:32:43 +1000

    Update to Vulkan 1.1.126

--- vkbind.h
+++ vkbind.h
@@ -1,6 +1,6 @@
 /*
 Vulkan API loader. Choice of public domain or MIT-0. See license statements at the end of this file.
-vkbind - v1.1.125.0 - 2019-10-14
+vkbind - v1.1.126.0 - 2019-10-21
 
 David Reid - davidreidsoftware@gmail.com
 */
@@ -138,7 +138,7 @@ will be added later. Let me know what isn't supported properly and I'll look int
 #define VK_VERSION_MAJOR(version) ((uint32_t)(version) >> 22)
 #define VK_VERSION_MINOR(version) (((uint32_t)(version) >> 12) & 0x3ff)
 #define VK_VERSION_PATCH(version) ((uint32_t)(version) & 0xfff)
-#define VK_HEADER_VERSION 125
+#define VK_HEADER_VERSION 126
 #define VK_NULL_HANDLE 0
 #define VK_DEFINE_HANDLE(object) typedef struct object##_T* object;
 #if !defined(VK_DEFINE_NON_DISPATCHABLE_HANDLE)
