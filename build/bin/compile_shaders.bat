baad6390f8e635d3ac9426379bc311722244e664
David Reid <mackron@gmail.com>
2019-11-16 11:37:28 +1000

    Restructure some files.

--- build/bin/compile_shaders.bat
+++ build/bin/compile_shaders.bat
@@ -1,5 +1,5 @@
-glslangValidator -V ..\..\examples\shaders\01_Triangle.glsl.vert -o 01_Triangle.vert.spirv
-dred -f file2chex 01_Triangle.vert.spirv -n g_VertexShaderData > 01_Triangle.vert.c
+glslangValidator -V ..\..\examples\shaders\01_Triangle\01_Triangle.glsl.vert -o ..\..\examples\resources\01_Triangle\01_Triangle.vert.spirv
+dred -f file2chex ..\..\examples\resources\01_Triangle\01_Triangle.vert.spirv -n g_VertexShaderData > 01_Triangle.vert.c
 
-glslangValidator -V ..\..\examples\shaders\01_Triangle.glsl.frag -o 01_Triangle.frag.spirv
-dred -f file2chex 01_Triangle.frag.spirv -n g_FragmentShaderData > 01_Triangle.frag.c
+glslangValidator -V ..\..\examples\shaders\01_Triangle\01_Triangle.glsl.frag -o ..\..\examples\resources\01_Triangle\01_Triangle.frag.spirv
+dred -f file2chex ..\..\examples\resources\01_Triangle\01_Triangle.frag.spirv -n g_FragmentShaderData > 01_Triangle.frag.c
