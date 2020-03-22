6ee92198614f4c3d99fbce53cbb90bb9793b38e0
David Reid <mackron@gmail.com>
2020-03-22 12:35:03 +1000

    Update triangle example to use the new VFS system.

--- build/bin/compile_shaders.bat
+++
@@ -1,5 +0,0 @@
-glslangValidator -V ..\..\examples\shaders\01_Triangle\01_Triangle.glsl.vert -o ..\..\examples\resources\01_Triangle\01_Triangle.vert.spirv
-dred -f file2chex ..\..\examples\resources\01_Triangle\01_Triangle.vert.spirv -n g_VertexShaderData > ..\..\examples\resources\01_Triangle\01_Triangle.vert.c
-
-glslangValidator -V ..\..\examples\shaders\01_Triangle\01_Triangle.glsl.frag -o ..\..\examples\resources\01_Triangle\01_Triangle.frag.spirv
-dred -f file2chex ..\..\examples\resources\01_Triangle\01_Triangle.frag.spirv -n g_FragmentShaderData > ..\..\examples\resources\01_Triangle\01_Triangle.frag.c
